a14f70b2b70fc7668688c6443963c4cd596bbc74
Nemolite <g25092011@mail.ru>
2023-09-03 06:24:35 +0300

    19082023

---
+++ z24.py
@@ -0,0 +1,7 @@
+mes,first_op,second = "Python","Java","Py"
+print(first_op in mes)
+print(second in mes)
+print(f'Строка {first_op} не входит в строку {mes}, поэтому {second}'.format(first_op=first_op,mes=mes,second=(first_op in mes)))
+
+
+
