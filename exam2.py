d2986fefeac7dc3ad2b1de995205f57707a14359
Nemolite <g25092011@mail.ru>
2023-09-03 07:04:21 +0300

    exam2

---
+++ exam2.py
@@ -0,0 +1,67 @@
+# Курс по 1С для преподавателей
+# Занятия от 22.08.2023 (онлайн занятие просмотрено в записи)
+# Решение практических задач
+
+# Задача №1
+print(5**2)
+print(25>24)
+print("Hello" + " " + "World")
+print(25%24)
+print(27//17)
+
+# Задача №2
+a = 47
+b = True
+c = "Hello Python"
+d = 11/3
+e = ["Python",23,False]
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+# Задача №3
+Cities = ["Moscow","St.Petersburg", "Vladimir"]
+Population = [17201245, 4520196, 3124075]
+print(Cities)
+print(Population)
+
+# Задача №4
+cities = {
+    "Moscow":1,
+    "St.Petersburg":2,
+    "Vladimir":3,
+    "Rostov":4,
+}
+cities.update(Omsk = 5,Novosibirsk = 6)
+print(cities)
+
+# Задача №5
+print("Данная программа способна выводить сообщение!")
+
+# Задача №6
+variable1 = int(input())
+variable2 = int(input())
+variable3 = int(input())
+
+print(variable1," ",variable2," ",variable3)
+
+# Задача №7
+variable_a = int(input())
+variable_b = int(input())
+variable_c = int(input())
+
+print(variable_a,"+",variable_b,"+",variable_c,"=",variable_a+variable_b+variable_c)
+
+# Задача №7
+x = float(input())
+y = float(input())
+z = float(input())
+s= x*y*z
+print(x,"*",y,"*",z,"=",s)
+
+
+
+
+
